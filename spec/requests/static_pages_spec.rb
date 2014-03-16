85ea472244e6e8d0ea098620810f34453ac91c34
Brandon McWilliams <brandon.mcwilliams@seagate.com>
2014-03-16 14:36:05 -0500

    variable interpolation (Exercise 2)
    Signed-off-by: Brandon McWilliams <brandon.mcwilliams@seagate.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -36,7 +38,7 @@ describe "Static pages" do
 
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
   
@@ -49,7 +51,7 @@ describe "Static pages" do
 
     it "should have the right title 'Contact'" do
       visit '/static_pages/concact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
